f7c8822de609f3938c687444949d58814c84863a
officialrkky <93412800+officialrkky@users.noreply.github.com>
2022-03-14 23:26:40 +0800

    Create skills.js

---
+++ skills.js
@@ -0,0 +1,37 @@
+$(document).ready(() => {
+  
+  $("#htmlCert").hide();
+  $("#cssCert").hide();
+  $("#jsCert").hide();
+  $("#pyCert").hide();
+  
+  $("#openHtmlCert").on('click', () =>{
+    $("#htmlCert").show();
+    $("#cssCert").hide();
+    $("#jsCert").hide();
+    $("#pyCert").hide();
+  });
+  
+  $("#openCssCert").on('click', () =>{
+    $("#htmlCert").hide();
+    $("#cssCert").show();
+    $("#jsCert").hide();
+    $("#pyCert").hide();
+  });
+  
+  $("#openJsCert").on('click', () =>{
+    $("#htmlCert").hide();
+    $("#cssCert").hide();
+    $("#jsCert").show();
+    $("#pyCert").hide();
+  });
+  
+  $("#openPyCert").on('click', () =>{
+    $("#htmlCert").hide();
+    $("#cssCert").hide();
+    $("#jsCert").hide();
+    $("#pyCert").show();
+  });
+
+
+});
