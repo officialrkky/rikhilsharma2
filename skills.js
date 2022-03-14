96a929376cc8c800eb97868a6c5a921cfbb3062c
officialrkky <93412800+officialrkky@users.noreply.github.com>
2022-03-14 23:37:41 +0800

    Update skills.js

--- skills.js
+++ skills.js
@@ -1,36 +1,36 @@
 $(document).ready(() => {
   
-  $("#htmlCert").hide();
-  $("#cssCert").hide();
-  $("#jsCert").hide();
-  $("#pyCert").hide();
+  $("#htmlCert").hide()
+  $("#cssCert").hide()
+  $("#jsCert").hide()
+  $("#pyCert").hide()
   
-  $("#openHtmlCert").on('click', () =>{
-    $("#htmlCert").show();
-    $("#cssCert").hide();
-    $("#jsCert").hide();
-    $("#pyCert").hide();
+  $('#openHtmlCert').on('click', () => {
+    $("#htmlCert").show()
+    $("#cssCert").hide()
+    $("#jsCert").hide()
+    $("#pyCert").hide()
   });
   
   $("#openCssCert").on('click', () =>{
-    $("#htmlCert").hide();
-    $("#cssCert").show();
-    $("#jsCert").hide();
-    $("#pyCert").hide();
+    $("#htmlCert").hide()
+    $("#cssCert").show()
+    $("#jsCert").hide()
+    $("#pyCert").hide()
   });
   
   $("#openJsCert").on('click', () =>{
-    $("#htmlCert").hide();
-    $("#cssCert").hide();
-    $("#jsCert").show();
-    $("#pyCert").hide();
+    $("#htmlCert").hide()
+    $("#cssCert").hide()
+    $("#jsCert").show()
+    $("#pyCert").hide()
   });
   
   $("#openPyCert").on('click', () =>{
-    $("#htmlCert").hide();
-    $("#cssCert").hide();
-    $("#jsCert").hide();
-    $("#pyCert").show();
+    $("#htmlCert").hide()
+    $("#cssCert").hide()
+    $("#jsCert").hide()
+    $("#pyCert").show()
   });
 
 
